d1236e3eb5f542531f0076544c5255aac715e35f
BlueVortex123 <77488945+BlueVortex123@users.noreply.github.com>
2024-09-02 15:01:16 +0300

    Update SamedayGetLockersRequest.php adding intl lockers router params

--- src/Sameday/Requests/SamedayGetLockersRequest.php
+++ src/Sameday/Requests/SamedayGetLockersRequest.php
@@ -15,15 +15,19 @@ class SamedayGetLockersRequest implements SamedayRequestInterface
      * @var array
      */
     protected $lockerIds;
+    protected $countryCode;
+    protected $locale;
 
     /**
      * SamedayGetLockersRequest constructor.
      *
      * @param array $lockerIds
      */
-    public function __construct(array $lockerIds = [])
+    public function __construct(array $lockerIds = [], $countryCode = 'RO', $locale = 'ro')
     {
         $this->lockerIds = $lockerIds;
+        $this->countryCode = $countryCode;
+        $this->locale = $locale;
     }
 
     /**
@@ -34,7 +38,7 @@ class SamedayGetLockersRequest implements SamedayRequestInterface
         return new SamedayRequest(
             true,
             'GET',
-            '/api/locker/lockers',
+            "/api/locker/lockers?_format=json&countryCode={$this->countryCode}&_locale={$this->locale}",
             count($this->lockerIds) > 0 ? ['lockersList' => implode(',', $this->lockerIds)] : []
         );
     }
